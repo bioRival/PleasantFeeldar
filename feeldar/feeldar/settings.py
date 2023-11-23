11f2947affd1478ae666cf5d6b1129d8198f75d3
dmitriy <kosheld@inbox.ru>
2023-11-23 22:55:01 +0500

    box

--- feeldar/feeldar/settings.py
+++ feeldar/feeldar/settings.py
@@ -84,14 +84,11 @@ WSGI_APPLICATION = 'feeldar.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ildar',
-        'USER': 'admin',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
+
 
 
 # Password validation
